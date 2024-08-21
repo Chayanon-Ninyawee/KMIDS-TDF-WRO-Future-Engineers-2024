b6d15f3d49052013e24ae860ba536ef0d2b6c65c
Garfieldcmix <chayanon.nin@gmail.com>
2024-08-21 14:44:37 +0700

    make raspi button work

--- RaspberrypiPython/robotcontroller.py
+++ RaspberrypiPython/robotcontroller.py
@@ -15,53 +15,58 @@ gpio = pgpio.PGPIO()
 
 def main():
     global robot_data
-    last_update_time = time.time()
-
-    # TODO: Check ultrasonic_info if it have any error
-
-    gpio.setup_gpio(13, gpio.Direction.INPUT, gpio.Pull.UP)
-    time.sleep(1)
-
-    while(gpio.input_gpio(13) == 1):
-        time.sleep(0.010)
-
-    time.sleep(1)
 
     try:
-        while True:
-            current_time = time.time()
-            delta_time = current_time - last_update_time
-            last_update_time = current_time
+        # TODO: Check ultrasonic_info if it have any error
 
-            robot_data.process_data()
+        gpio.setup_gpio(13, gpio.Direction.INPUT, gpio.Pull.UP)
+        time.sleep(1)
 
-            full_image = robot_data.get_image()
-            image = full_image[IMAGE_HEIGHT:, :]
+        while(gpio.input_gpio(13) == 1):
+            time.sleep(0.010)
 
-            ultrasonic_info = robot_data.get_ultrasonic_data()
-            gyro_info = robot_data.get_gyro_data()
+        time.sleep(1)
 
-            if not delta_time == 0:
-                #print(f'{1/delta_time} {ultrasonic_info}')
-                pass
+        last_update_time = time.time()
+        try:
+            while True:
+                current_time = time.time()
+                delta_time = current_time - last_update_time
+                last_update_time = current_time
 
-            result = process_data_open(ultrasonic_info, gyro_info, image, delta_time)
-            # result = process_data_obstacle(ultrasonic_info, gyro_info, image, delta_time)
-            # result = (0, 0)
+                robot_data.process_data()
 
-            speed_target = 0
-            steering_percent = 0 
-            if type(result) is bool:
-                if result == False:
-                    robot_data.send_data(0.0, 0.0)
-                    break
-            else:
-                speed_target, steering_percent = result
+                full_image = robot_data.get_image()
+                image = full_image[IMAGE_HEIGHT:, :]
 
-            robot_data.send_data(speed_target, steering_percent)
+                ultrasonic_info = robot_data.get_ultrasonic_data()
+                gyro_info = robot_data.get_gyro_data()
 
-    except Exception as e: print(e)
-    finally:
+                if not delta_time == 0:
+                    #print(f'{1/delta_time} {ultrasonic_info}')
+                    pass
+
+                result = process_data_open(ultrasonic_info, gyro_info, image, delta_time)
+                # result = process_data_obstacle(ultrasonic_info, gyro_info, image, delta_time)
+                # result = (0, 0)
+
+                speed_target = 0
+                steering_percent = 0 
+                if type(result) is bool:
+                    if result == False:
+                        robot_data.send_data(0.0, 0.0)
+                        break
+                else:
+                    speed_target, steering_percent = result
+
+                robot_data.send_data(speed_target, steering_percent)
+
+        except Exception as e: print(e)
+        finally:
+            cv2.destroyAllWindows()
+            robot_data.close()
+    except Exception as e:
+        print(e)
         cv2.destroyAllWindows()
         robot_data.close()
 
