d602f16c5c9eebf507d47b0a3530cd7898647b22
Garfieldcmix <chayanon.nin@gmail.com>
2024-10-27 13:10:53 +0000

    Make RPLidar C1 work

--- RaspberryPi5/TestV1/src/main.cpp
+++ RaspberryPi5/TestV1/src/main.cpp
@@ -1,74 +1,122 @@
-#include <cstdlib>
+#include <iostream>
+#include <vector>
 #include <csignal>
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
-#include <wiringPi.h>
+#include <chrono>
+#include <thread>
 
-#define	LED_PIN	15
-#define BUTTON_PIN 16
+#include "rplidar.h"
 
+using namespace rp::standalone::rplidar;
 
-volatile bool isStarted = false;
+bool isRunning = true;
 
-void mainButtonInterrupt(void) {
-  isStarted = true;
+void interuptHandler(int signum)
+{
+  std::cout << "\nInterrupt signal (" << signum << ") received. Stopping the scan." << std::endl;
+  isRunning = false;
 }
 
+int main(int argc, char *argv[])
+{
+  signal(SIGINT, interuptHandler);
 
-void cleanup() {
-  pinMode(LED_PIN, INPUT);
-  pullUpDnControl(LED_PIN, PUD_OFF);
-
-  pinMode(BUTTON_PIN, INPUT);
-  pullUpDnControl(BUTTON_PIN, PUD_OFF);
-}
-
-void exiting() {
-  cleanup();
-}
-
-void exiting(int i) {
-  exit(0);
-}
-
-int main(int argc, char * argv[]) {
-  if (wiringPiSetup() < 0) {
-    printf("Failed to initialize wiringPi: %s\n", strerror(errno));
-    return 1;
+  RPlidarDriver *rplidarDriver = RPlidarDriver::CreateDriver(DRIVER_TYPE_SERIALPORT);
+  if (!rplidarDriver)
+  {
+    std::cerr << "Failed to create RPLidar driver." << std::endl;
+    return -1;
   }
 
-  pinMode(BUTTON_PIN, INPUT);
-  pullUpDnControl(BUTTON_PIN, PUD_UP);
-
-  if (wiringPiISR(BUTTON_PIN, INT_EDGE_FALLING, &mainButtonInterrupt) < 0) {
-    printf("Failed to initialize ISR: %s\n", strerror(errno));
-    return 1;
+  u_result result = rplidarDriver->connect("/dev/ttyAMA0", 460800);
+  if (IS_FAIL(result))
+  {
+    std::cerr << "Failed to connect to the RPLidar." << std::endl;
+    RPlidarDriver::DisposeDriver(rplidarDriver);
+    return -1;
   }
 
-  pinMode(LED_PIN, OUTPUT);
-
-
-  signal(SIGINT, exiting);
-  signal(SIGABRT, exiting);
-  signal(SIGTERM, exiting);
-  signal(SIGTSTP, exiting);
-  atexit(exiting);
-  
-
-  printf("Waiting for button press...\n");
-  while (!isStarted) {
-    delay(50);
+  rplidar_response_device_info_t deviceInfo;
+  result = rplidarDriver->getDeviceInfo(deviceInfo);
+  if (IS_OK(result))
+  {
+    std::cout << "RPLidar Device Info:" << std::endl;
+    std::cout << " - Model: " << static_cast<int>(deviceInfo.model) << std::endl;
+    std::cout << " - Firmware Version: " << (deviceInfo.firmware_version >> 8) << "." << (deviceInfo.firmware_version & 0xFF) << std::endl;
+    std::cout << " - Hardware Version: " << static_cast<int>(deviceInfo.hardware_version) << std::endl;
+    std::cout << " - Serial Number: ";
+    for (int i = 0; i < 16; ++i)
+    {
+      printf("%02X", deviceInfo.serialnum[i]);
+    }
+    std::cout << std::endl;
+  }
+  else
+  {
+    std::cerr << "Failed to retrieve device information." << std::endl;
   }
 
-  while (true) {
-    digitalWrite (LED_PIN, HIGH);
-    printf("LED ON!\n");
-    delay(1000);
-    digitalWrite (LED_PIN, LOW);
-    printf("LED OFF!\n");
-    delay(1000);
+  rplidarDriver->startMotor();
+
+  std::vector<LidarScanMode> scanModes;
+  rplidarDriver->getAllSupportedScanModes(scanModes);
+  if (scanModes.empty())
+  {
+    std::cerr << "No supported scan modes available." << std::endl;
+    rplidarDriver->stopMotor();
+    RPlidarDriver::DisposeDriver(rplidarDriver);
+    return -1;
   }
-  
+
+  std::cout << "Available scan modes:" << std::endl;
+  for (const auto &mode : scanModes)
+  {
+    std::cout << " - Mode ID: " << mode.id << ", us per Sample: " << mode.us_per_sample << ", Max Distance: " << mode.max_distance << "m" << std::endl;
+  }
+
+  result = rplidarDriver->startScanExpress(false, scanModes[0].id);
+  if (IS_FAIL(result))
+  {
+    std::cerr << "Failed to start scan." << std::endl;
+    rplidarDriver->stopMotor();
+    RPlidarDriver::DisposeDriver(rplidarDriver);
+    return -1;
+  }
+
+  rplidar_response_measurement_node_hq_t nodes[8192];
+  size_t nodeCount = sizeof(nodes)/sizeof(rplidar_response_measurement_node_hq_t);
+
+
+  auto startTime = std::chrono::high_resolution_clock::now();
+
+  while (isRunning)
+  {
+    result = rplidarDriver->grabScanDataHq(nodes, nodeCount, 0);
+    if (IS_FAIL(result))
+    {
+      // std::cerr << "Failed to grab scan data." << std::endl;
+      continue;
+    }
+
+    rplidarDriver->ascendScanData(nodes, nodeCount);
+    for (size_t i = 0; i < nodeCount; ++i)
+    {
+      float angle = nodes[i].angle_z_q14 * 90.f / (1 << 14);
+      float distance = nodes[i].dist_mm_q2 / 1000.f / (1 << 2);
+      std::cout << "Angle: " << angle << " Dist: " << distance << " m" << nodeCount << std::endl;
+    }
+
+    auto endTime = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsedTime = endTime - startTime;
+    double refreshRate = 1 / elapsedTime.count(); // scans per second
+    std::cout << " Node Count: " << nodeCount << " Refresh Rate: " << refreshRate << " scans/second" << std::endl;
+
+    startTime = endTime;
+  }
+
+  rplidarDriver->stop();
+  rplidarDriver->stopMotor();
+  rplidarDriver->disconnect();
+  RPlidarDriver::DisposeDriver(rplidarDriver);
+
   return 0;
 }
