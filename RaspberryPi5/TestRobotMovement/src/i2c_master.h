24d31a84f23395a1dfd144b2f204314cd5aa5a32
Garfieldcmix <chayanon.nin@gmail.com>
2024-11-19 07:49:39 +0000

    Add docstring

--- RaspberryPi5/TestRobotMovement/src/i2c_master.h
+++ RaspberryPi5/TestRobotMovement/src/i2c_master.h
@@ -47,13 +47,69 @@ enum Command : uint8_t {
   SKIP_CALIB = 0x04
 };
 
-// Function declarations
+
+/**
+ * @brief Initializes the I2C master communication with the specified slave address.
+ *
+ * @param slave_address The 7-bit address of the I2C slave device.
+ * @return File descriptor for the I2C device, or -1 on failure.
+ */
 int i2c_master_init(uint8_t slave_address);
+
+/**
+ * @brief Sends a command to the I2C slave device.
+ *
+ * @param fd File descriptor of the I2C device.
+ * @param command The command byte to send.
+ */
 void i2c_master_send_command(int fd, uint8_t command);
+
+/**
+ * @brief Sends data to a specific register on the I2C slave device.
+ *
+ * @param fd File descriptor of the I2C device.
+ * @param reg Register address to write the data to.
+ * @param data Pointer to the data buffer to send.
+ * @param len Length of the data buffer.
+ */
 void i2c_master_send_data(int fd, uint8_t reg, uint8_t *data, uint8_t len);
+
+/**
+ * @brief Reads data from a specific register on the I2C slave device.
+ *
+ * @param fd File descriptor of the I2C device.
+ * @param reg Register address to read the data from.
+ * @param data Pointer to the buffer to store the read data.
+ * @param len Number of bytes to read.
+ */
 void i2c_master_read_data(int fd, uint8_t reg, uint8_t *data, uint8_t len);
+
+/**
+ * @brief Reads log data from the I2C slave device using the default log size.
+ *
+ * @param fd File descriptor of the I2C device.
+ * @param logs Pointer to the buffer to store the logs.
+ */
 void i2c_master_read_logs(int fd, uint8_t *logs);
+
+/**
+ * @brief Reads log data from the I2C slave device with a specified log size.
+ *
+ * @param fd File descriptor of the I2C device.
+ * @param logs Pointer to the buffer to store the logs.
+ * @param len Number of bytes to read from the log buffer.
+ */
 void i2c_master_read_logs(int fd, uint8_t *logs, size_t len);
+
+/**
+ * @brief Prints the logs in a readable format.
+ *
+ * @param logs Pointer to the buffer containing the log data.
+ * @param len Length of the log buffer.
+ */
 void i2c_master_print_logs(uint8_t *logs, size_t len);
+
+
+
 
 #endif // I2C_MASTER_H
